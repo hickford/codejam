48855d01692c2a05b3a666ed798c916d62665104
Matt Hickford <matt.hickford@gmail.com>
2011-09-08 11:35:12 +0100

    fileinput.input()

--- 2011/3/straights/straights.py
+++ 2011/3/straights/straights.py
@@ -1,25 +1,9 @@
 #!python
 # Dire Straights http://code.google.com/codejam/contest/dashboard?c=1158485#s=p1
-from __future__ import division
-import math
-from collections import deque
-import sys
-from optparse import OptionParser
-usage = "usage: %prog input"
-parser = OptionParser(usage=usage)
-(options, args) = parser.parse_args()
-if args:
-    if args[0] == "-":
-        f = sys.stdin
-    else:
-        f = open(args[0])
-elif not sys.stdin.isatty():
-    f = sys.stdin
-else:
-    parser.error("Need input from file or stdin")
-
+import fileinput
+f = fileinput.input()
 T = int(f.readline())
-                      
+
 for i in range(1,T+1):
     line = [int(x) for x in f.readline().split()]
     N = line[0]
@@ -40,5 +24,3 @@ for i in range(1,T+1):
     if straights:
         answer = min(len(straight) for straight in straights)
     print "Case #%d: %s" % (i,answer)
-        
-        
