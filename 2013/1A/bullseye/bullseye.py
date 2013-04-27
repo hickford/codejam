41042c54945ef09db9450d4405c93f6d10b9d5e9
Matt Hickford <matt.hickford@gmail.com>
2013-04-27 18:11:23 +0100

    second solution using quadratic equation

--- 2013/1A/bullseye/bullseye.py
+++ 2013/1A/bullseye/bullseye.py
@@ -2,9 +2,24 @@
 # https://code.google.com/codejam/contest/2418487/dashboard#s=p0
 # Maria has been hired by the Ghastly Chemicals Junkies (GCJ) company to help them manufacture bullseyes. A bullseye consists of a number of concentric rings (rings that are centered at the same point), and it usually represents an archery target. GCJ is interested in manufacturing black-and-white bullseyes. 
 
+from __future__ import division
+import math
+
 def f(n,r):
 	"""Area of bullseye with n rings, inner radius r, as a multiple of pi."""
 	return 2*n**2 + n*(2*r-1)
+
+def solve2(r,t):
+	"""Solve using quadratic equation"""
+	import gmpy
+	from gmpy import mpz
+
+	a = 2
+	b = 2*r - 1
+	c = -t
+
+	x = (-b + mpz(b**2 - 4*a*c).sqrt()) // (2*a) 
+	return int(x)
 
 assert f(0,0) == 0
 assert f(0,1) == 0
@@ -49,6 +64,8 @@ assert binary_search(lambda x: x, 6) == 6
 assert binary_search(lambda x: x**2, 10) == 3
 assert binary_search(lambda x: x+10, 5) == 0
 
+
+
 if __name__ == "__main__":
 	import fileinput
 	stdin = fileinput.input()
@@ -58,4 +75,5 @@ if __name__ == "__main__":
 	for case in range(1,T+1):
 		r,t = [int(x) for x in stdin.readline().split()]
 		answer = solve(r,t)
+		assert answer == solve2(r,t)
 		print "Case #%d: %s" % (case, answer)
