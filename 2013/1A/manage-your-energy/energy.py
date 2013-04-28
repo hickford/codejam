07041e9dfdb88fa6f9b131fc755beacaf3f0b5e5
Matt Hickford <matt.hickford@gmail.com>
2013-04-28 19:31:43 +0100

    greedy solution (slow)

--- 2013/1A/manage-your-energy/energy.py
+++ 2013/1A/manage-your-energy/energy.py
@@ -3,6 +3,39 @@
 # You've got a very busy calendar today, full of important stuff to do. You worked hard to prepare and make sure all the activities don't overlap. Now it's morning, and you're worried that despite all of your enthusiasm, you won't have the energy to do all of this with full engagement.
 import collections
 
+def solve_greedy(E, R, values):
+	"Solve using greedy algorithm."
+	score = 0
+	energy = [E for v in values] # energy left after activity [i] before regeneration
+	assigned = [0 for v in values] # energy before activity i that has been assigned for use somewhere.
+
+	best_to_worse = list(reversed(sorted(range(len(values)), key = lambda i: values[i])))
+
+	for i in best_to_worse:
+		while assigned[i] < E and not (i > 0 and assigned[i] >= energy[i-1] + R):
+			assigned[i] += 1
+			energy[i] -= 1
+			score += values[i]
+
+		for j in range(i+1, len(values)):
+			# energy[j] = min(energy[j], energy[j-1]+R)
+			if energy[j] > energy[j-1]+R:
+				energy[j] = energy[j-1]+R
+			else:
+				break
+
+		for j in reversed(range(i)):
+			# assigned[j] = max(assigned[j], assigned[j+1]-R)
+			if assigned[j] < assigned[j+1] - R:
+				assigned[j] = assigned[j+1] - R
+			else:
+				break
+
+		assert all(0<=e<=E for e in energy)
+		assert all(0<=a<=E for a in assigned)
+
+	return score
+
 def first(collection):
 	for x in collection:
 		return x
@@ -54,3 +87,4 @@ if __name__ == "__main__":
 		assert len(values) == N
 		answer = solve(E, R, values)
 		print "Case #%d: %s" % (case, answer)
+		# assert solve(E, R, values) == solve_greedy(E, R, values)
