61ad81a1933ec6d1ada7ed55c93ebfeb7f294975
Matt Hickford <matt.hickford@gmail.com>
2015-01-25 19:26:13 +0000

    publish package

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+setup(
+    name='codejamhelpers',
+
+    # useful: python setup.py sdist bdist_wheel upload
+    version='0.0.1',
+
+    description='Helper algorithms for solving Google Code Jam problems',
+
+    url='https://github.com/hickford/codejam',
+
+    author='Matt Hickford',
+    author_email='matt.hickford@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    packages=['codejamhelpers'],
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
+    install_requires=[],
+)
