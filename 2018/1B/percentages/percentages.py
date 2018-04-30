acd7859e391fc3ffe9175eec12451a595d45229a
M Hickford <mirth.hickford@gmail.com>
2018-04-30 13:01:57 +0100

    better?

--- 2018/1B/percentages/percentages.py
+++ 2018/1B/percentages/percentages.py
@@ -4,23 +4,29 @@ f = fileinput.input()
 def input():
     return f.readline()
 
-def percentage(a, b):
-    quotient, remainder = divmod(a*100,b)
-    if remainder >= (b+1)//2:
-        quotient += 1
-    return quotient
+def divide_rounding(p, q):
+    return (p + q//2) // q
 
-def how_many_to_make_round_up(a, b):
-    quotient, remainder = divmod(a*100,b)
-    if remainder >= (b+1)//2:
-        return 0
-    else:
-        return (b+1)//2 - remainder
+def percentage(p, q):
+    return divide_rounding(100*p, q)
 
 assert percentage(5, 100) == 5
 assert percentage(5, 1000) == 1
 assert percentage(5, 1001) == 0
 assert percentage(6, 1001) == 1
+
+def divide_rounding_up(p, q):
+    return (p + q-1) // q
+
+from math import inf
+
+def extra_votes_to_round_up(p, q):
+    remainder = p*100 % q
+    threshold = (q+1)//2
+    if remainder >= threshold:
+        return inf
+    else:
+        return divide_rounding_up(threshold-remainder, 100)
 
 T = int(input())
 for case in range(1, T+1):
@@ -31,19 +37,13 @@ for case in range(1, T+1):
 
     while sum(votes) < N:
         remaining = N - sum(votes)
-        round_downs = [i for i, v in enumerate(votes) if how_many_to_make_round_up(v, N)]
         if votes[-1] != 0:
             votes.append(0)
-        if round_downs:
-            key = lambda i: how_many_to_make_round_up(votes[i], N)
-            i = min(round_downs, key=key)
-            spend = key(i)
-            if spend <= remaining:
-                votes[i] += spend
-                continue
-        spend = how_many_to_make_round_up(0, N)
-        spend = min(spend, remaining)
-        votes.append(spend)
+        key = lambda i: extra_votes_to_round_up(votes[i], N)
+        i = min(range(len(votes)), key=key)
+        spend = key(i)
+        votes[i] += min(spend, remaining)
+        # print(votes, file=stderr)
 
     solution = sum(percentage(v, N) for v in votes)    
     print("Case #{}: {}".format(case, solution))
